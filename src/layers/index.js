8ef82fa1d6ff4cd2344df2f903a5f0f9596d73de
王阁元 <wanggeyuan@wanggeyuandeMacBook-Pro.local>
2018-12-25 10:50:33 +0800

    update heatmap

--- src/layers/index.js
+++ src/layers/index.js
@@ -4,6 +4,6 @@ export {PolygonLayer} from "./PolygonLayer.js"
 export {MarkerLayer} from "./MarkerLayer.js"
 export {ODLayer} from './ODLayer'
 export {PolylineLayer} from './PolylineLayer.js'
-// export {heatmapOverlay} from './heatmap.js'
+export {heatmapOverlay} from './heatmap.js'
 
 export * from './vector/index'
